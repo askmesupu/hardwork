a9a55c7d45a06c73baf403fa91f1832cae9df08c
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 23:58:09 +0600

    Create MyHeart.js

---
+++ src/pages/MyHeart.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HeartAnimation from "../components/HeartAnimation";
+
+export default function MyHeart() {
+  return (
+    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+      <HeartAnimation />
+    </div>
+  );
+}
