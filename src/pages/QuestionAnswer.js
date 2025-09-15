631a5aa8c3c454d6b03816c62221644181f73b00
askmesupu <ask.me.pirated@gmail.com>
2025-09-16 00:29:46 +0600

    Update QuestionAnswer.js

--- src/pages/QuestionAnswer.js
+++ src/pages/QuestionAnswer.js
@@ -1,53 +1,39 @@
 import React, { useState } from "react";
 
-export default function QuestionAnswer() {
-  const [noPosition, setNoPosition] = useState({ top: "50%", left: "40%" });
-  const [yesClicked, setYesClicked] = useState(false);
-  const [popupImg, setPopupImg] = useState(null);
+export default function AnswerQuestion() {
+  const [showYesPic, setShowYesPic] = useState(false);
+  const [showNoPic, setShowNoPic] = useState(false);
 
-  const handleNoClick = () => {
-    const top = Math.random() * 60 + 20 + "%";
-    const left = Math.random() * 60 + 20 + "%";
-    setNoPosition({ top, left });
-    setPopupImg("https://i.imgur.com/tpIx24B.png"); // Dudu crying image
-    setTimeout(() => setPopupImg(null), 1500);
+  const handleYes = () => {
+    setShowYesPic(true);
+    setTimeout(() => setShowYesPic(false), 2000);
+    alert("I love you baby");
   };
 
-  const handleYesClick = () => {
-    setYesClicked(true);
-    setPopupImg("https://i.imgur.com/nXvrTyh.jpeg"); // Dudu happy image
-    setTimeout(() => {
-      setPopupImg(null);
-      setYesClicked(false);
-    }, 2000);
+  const handleNo = () => {
+    setShowNoPic(true);
+    setTimeout(() => setShowNoPic(false), 2000);
   };
 
   return (
-    <div className="subpage-content" style={{ position: "relative" }}>
+    <div className="subpage-content">
       <h2>Will you stay forever with me?</h2>
-      <div style={{ marginTop: "2rem", position: "relative" }}>
-        <button onClick={handleYesClick} className="subpage-button">Yes</button>
-        <button
-          onMouseEnter={handleNoClick}
-          style={{ top: noPosition.top, left: noPosition.left, position: "absolute" }}
-          className="subpage-button"
-        >
-          No
-        </button>
+      <div className="answer-buttons">
+        <button style={{ fontSize: "1.5rem" }} onClick={handleYes}>Yes</button>
+        <button style={{ fontSize: "1.5rem" }} onClick={handleNo}>No</button>
       </div>
-      {yesClicked && (
-        <div style={{
-          position: "absolute",
-          top: "70%",
-          left: "50%",
-          transform: "translateX(-50%)",
-          color:"white",
-          fontSize:"1.5rem"
-        }}>
-          I love you baby
+
+      {showYesPic && (
+        <div style={{ position: "fixed", top: "20%", left: "50%", transform: "translateX(-50%)", zIndex: 1000 }}>
+          <img src="https://i.imgur.com/nXvrTyh.jpeg" alt="Yes" style={{ width: "200px" }} />
         </div>
       )}
-      {popupImg && <img src={popupImg} alt="popup" className="popup-img" />}
+
+      {showNoPic && (
+        <div style={{ position: "fixed", top: "20%", left: "50%", transform: "translateX(-50%)", zIndex: 1000 }}>
+          <img src="https://i.imgur.com/tpIx24B.png" alt="No" style={{ width: "200px" }} />
+        </div>
+      )}
     </div>
   );
-    }
+        }
