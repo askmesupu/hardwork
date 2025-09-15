97fb16b26aa3d6a5944639ae04dd782761f84e95
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 23:55:15 +0600

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="subpage-content">
+      <div>
+        <header>
+          <h1>Supto ❤️ Shiropa</h1>
+          <p>
+            In 2023, Supto first fell in love with Shiropa at first sight. He prayed to God to make her his. After Durga Puja on 20 October 2024, their relationship officially started. Since then, their love grows day by day — simple, true and beautiful.
+          </p>
+        </header>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}>
+          <img src="https://i.imgur.com/8sdznQ1.jpeg" alt="Bubu" className="circle-pic" />
+          <img src="https://i.imgur.com/OGgciNc.png" alt="Dudu" className="circle-pic" />
+        </div>
+      </div>
+    </div>
+  );
+    }
