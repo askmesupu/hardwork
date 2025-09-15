74d43dedf2476ef5d01f14ecb458f3da8ff5575f
askmesupu <ask.me.pirated@gmail.com>
2025-09-15 23:55:57 +0600

    Create English.js

---
+++ src/pages/English.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function English() {
+  return (
+    <div className="subpage-content">
+      <p>
+        From the depths of my soul, I know Shiropa is my eternal light. Her smile washes away every sorrow; her presence sets the rhythm of my heart. I love her beyond words, beyond time — today, tomorrow and for always.
+      </p>
+    </div>
+  );
+}
