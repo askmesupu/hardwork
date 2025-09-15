4b2568882261559a1bebf1efca7d489ebaf17211
askmesupu <ask.me.pirated@gmail.com>
2025-09-16 00:58:16 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 // src/App.js
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeartAnimation from "./components/HeartAnimation";
 
-// Pages
 import Home from "./pages/Home";
 import English from "./pages/English";
 import Bangla from "./pages/Bangla";
@@ -12,45 +11,21 @@ import Hindi from "./pages/Hindi";
 import MyHeart from "./pages/MyHeart";
 import QuestionAnswer from "./pages/QuestionAnswer";
 
-function AppWrapper() {
+function App() {
   return (
     <Router>
-      <App />
-    </Router>
-  );
-}
-
-function App() {
-  const [showHeart, setShowHeart] = useState(false);
-  const [navigateTo, setNavigateTo] = useState(null);
-
-  const handleNavigation = (path) => {
-    setNavigateTo(path);
-    setShowHeart(true);
-  };
-
-  const handleHeartEnd = (navigate) => {
-    setShowHeart(false);
-    if (navigate) {
-      navigate(navigateTo);
-      setNavigateTo(null);
-    }
-  };
-
-  return (
-    <>
-      <Navbar onNavClick={handleNavigation} />
-      {showHeart && <HeartAnimation onAnimationEnd={handleHeartEnd} />}
+      <HeartAnimation /> {/* Full screen heart animation component */}
+      <Navbar /> {/* Navbar always visible */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/english" element={<English />} />
         <Route path="/bangla" element={<Bangla />} />
         <Route path="/hindi" element={<Hindi />} />
-        <Route path="/myheart" element={<MyHeart />} />
-        <Route path="/question" element={<QuestionAnswer />} />
+        <Route path="/my-heart" element={<MyHeart />} />
+        <Route path="/question-answer" element={<QuestionAnswer />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
-export default AppWrapper;
+export default App;
